c8bb3fc6f9adad353b5d2bfd48c834a7a7557889
Piotr Zientara <riozshow@gmail.com>
2024-02-16 20:10:01 +0100

    Redux fix

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -14,9 +14,9 @@ const rootReducer = combineReducers({
 const store = createStore(
   rootReducer,
   compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+    applyMiddleware(thunk)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
